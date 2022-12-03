d7dfdf99b608e28459354854130ce290d07cb95f
Sam Zun <zunsamuel97@gmail.com>
2022-12-03 00:04:59 -0600

    added docstrings to the class

--- model_controller.py
+++ model_controller.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Class to control all model-related interactions 
 class Model_Controller:
     
-    def __init__(self, model, model_type, data):
+    """
+        Class to control all model-related interactions
+    """
+    
+    # Constructor
+    def __init__(self, model, model_name, data):
+        
+        """
+            Class Constructor
+            
+            @Parameters: 
+                - model: sklearn classifier
+                - model_name: name of the sklearn model
+                - data: dictionary with train & test data
+        """
+        
+        # Setting all class variables
         self.model = model
-        self.model_type = model_type
+        self.model_name = model_name
         self.X_train_scaled = data['X_train_scaled']
         self.X_test_scaled = data['X_test_scaled']
         self.X_test = data['X_test']
@@ -17,29 +34,57 @@ class Model_Controller:
         self.returns = 'Returns'
         self.actual_returns = 'Actual ' + self.returns
         self.strategy_returns = 'Strategy ' + self.returns
-        self.on_init()
-        
-    def on_init(self):
+        # Setting predictions
         self.set_predictions()
         
     def set_predictions(self):
+        
+        """
+            Instantiates the model, fits the model and sets the predictions.
+        """
+        
         self.pred = self.model(random_state=1).fit(self.X_train_scaled,self.y_train).predict(self.X_test_scaled)
         
     def get_predictions(self):
+        
+        """
+            Returns the predictions
+        """
+        
         return self.pred
     
     def print_report(self):
+        
+        """
+            Prints the classification report
+        """
+        
         print(self.y_test,self.pred)
         
     def get_df(self):
+        
+        """
+            Creates and returns the predictions DataFrame
+        """
+        
         self.df = pd.DataFrame({'Predicted': self.pred, self.actual_returns: self.signals_df[self.actual_returns]}, index=self.X_test.index)
         self.df[self.strategy_returns] = self.df[self.actual_returns] * self.pred
         return self.df
     
     def display_head_tail(self):
+        
+        """
+            Displays the head and tail of the predictions DataFrame
+        """
+        
         display(self.df.head(),self.df.tail())
         
     def plot_save_returns(self):
+        
+        """
+            Plots the Actual Returns vs the Strategy Returns and saves the plot as an image
+        """
+        
         bold = 'bold'
         label_fontsize,ticks_fontsize = 18,14
         
@@ -48,11 +93,11 @@ class Model_Controller:
         legend = plt.legend(title=self.returns, fontsize=12, loc=4)
         plt.setp(legend.get_title(),fontsize=16, fontweight=bold)
         
-        plt.title(f'Actual vs {self.model_type} {self.strategy_returns} 2015-2020', fontsize=24, fontweight=bold)
+        plt.title(f'Actual vs {self.model_name} {self.strategy_returns} 2015-2020', fontsize=24, fontweight=bold)
         plt.grid(color='#eeeeee', linestyle='-', linewidth=1.25)
         plt.xlabel('Date', fontsize=label_fontsize, fontweight=bold)
         plt.ylabel(self.returns, fontsize=label_fontsize, fontweight=bold)
         plt.xticks(fontsize=ticks_fontsize,fontweight=bold)
         plt.yticks(fontsize=ticks_fontsize,fontweight=bold)
         
-        plt.savefig(f'./assets/images/returns_{self.model_type}.png');
+        plt.savefig(f'./assets/images/returns_{self.model_name}.png');
