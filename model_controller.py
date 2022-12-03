87386003c08dd093dbbcb6d9826ad28ae55bd0e8
Sam Zun <zunsamuel97@gmail.com>
2022-12-02 23:56:55 -0600

    made class dryer

--- model_controller.py
+++ model_controller.py
@@ -14,6 +14,9 @@ class Model_Controller:
         self.signals_df = data['signals_df']
         self.pred = None
         self.df = None
+        self.returns = 'Returns'
+        self.actual_returns = 'Actual ' + self.returns
+        self.strategy_returns = 'Strategy ' + self.returns
         self.on_init()
         
     def on_init(self):
@@ -29,24 +32,27 @@ class Model_Controller:
         print(self.y_test,self.pred)
         
     def get_df(self):
-        self.df = pd.DataFrame({'Predicted': self.pred, 'Actual Returns': self.signals_df['Actual Returns']}, index=self.X_test.index)
-        self.df['Strategy Returns'] = self.df['Actual Returns'] * self.pred
+        self.df = pd.DataFrame({'Predicted': self.pred, self.actual_returns: self.signals_df[self.actual_returns]}, index=self.X_test.index)
+        self.df[self.strategy_returns] = self.df[self.actual_returns] * self.pred
         return self.df
     
     def display_head_tail(self):
         display(self.df.head(),self.df.tail())
         
     def plot_save_returns(self):
-        (1 + self.df[['Actual Returns','Strategy Returns']]).cumprod().plot(
-            figsize=(20,10), 
-            linestyle=(0, (3, 1, 1, 1, 1, 1)),
-        );
-        plt.title(f'Actual vs {self.model_type} Strategy Returns 2015-2020', fontsize=24, fontweight='bold')
+        bold = 'bold'
+        label_fontsize,ticks_fontsize = 18,14
+        
+        (1 + self.df[[self.actual_returns, self.strategy_returns]]).cumprod().plot(figsize=(20,10), linestyle=(0, (3, 1, 1, 1, 1, 1)));
+        
+        legend = plt.legend(title=self.returns, fontsize=12, loc=4)
+        plt.setp(legend.get_title(),fontsize=16, fontweight=bold)
+        
+        plt.title(f'Actual vs {self.model_type} {self.strategy_returns} 2015-2020', fontsize=24, fontweight=bold)
         plt.grid(color='#eeeeee', linestyle='-', linewidth=1.25)
-        legend = plt.legend(title='Returns', fontsize=12, loc=4)
-        plt.setp(legend.get_title(),fontsize=16, fontweight='bold')
-        plt.xlabel('Date', fontsize=18, fontweight='bold')
-        plt.ylabel('Returns', fontsize=18, fontweight='bold')
-        plt.xticks(fontsize=14,fontweight='bold')
-        plt.yticks(fontsize=14,fontweight='bold')
+        plt.xlabel('Date', fontsize=label_fontsize, fontweight=bold)
+        plt.ylabel(self.returns, fontsize=label_fontsize, fontweight=bold)
+        plt.xticks(fontsize=ticks_fontsize,fontweight=bold)
+        plt.yticks(fontsize=ticks_fontsize,fontweight=bold)
+        
         plt.savefig(f'./assets/images/returns_{self.model_type}.png');
